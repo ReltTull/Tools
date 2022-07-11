002d77d707b1d6ea5dd01a10860a738e817a1fdb
ReltTull <92668493+ReltTull@users.noreply.github.com>
2022-07-11 19:30:52 +0300

    comments added

--- TreeBlock.java
+++ TreeBlock.java
@@ -1,5 +1,9 @@
 public class TreeBlock extends Materials{
+    // класс деревянный блок, с исходным состоянием state = 100. Этот блок будет пилиться пилой saw.
     int state = 100;
+    public TreeBlock(int st) {
+        st = state;
+    }
 
     @Override
     void showInfo() {
