a729c7a94fe6e233cb8fef205bcbafd82ce166dd
ReltTull <92668493+ReltTull@users.noreply.github.com>
2022-07-11 19:30:13 +0300

    comments added

--- Saw.java
+++ Saw.java
@@ -1,12 +1,14 @@
 public class Saw extends Tool{
+    // класс пило, с методом cut для работы с блоком дерева, где уменьшается TreeBlock.state.
     TreeBlock tr;
     @Override
     void using() {
-        cut(tr, 20);
+        cut(tr);
 
     }
-    void cut(TreeBlock tr, int cut) {
-        tr.state -= cut;
+
+    void cut(TreeBlock block) {
+        block.state /= 2;
         System.out.print("Tree block size: " + tr.state);
     }
 }
